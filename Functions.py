fa621b99dd6f084c227782441e20cbcaea4af51a
saeedfalowo <saeedfalowo@users.github.com>
2020-01-26 21:31:28 +0000

    Image Processing Techniques exploration

--- Functions.py
+++ Functions.py
@@ -12,3 +12,16 @@ def CreateCheckerBoard(n):
     print('Saving the checkerboard image in the Images folder as Checkerboard('+str(n*2)+').png ....')
     fig.savefig('Images/Checkerboard('+str(n*2)+').png')
     return plt.show()
+
+def Orignal_v_Filtered(img,filt_img):
+    import matplotlib.pyplot as plt
+    plt.subplot(1,2,1)
+    plt.imshow(img, cmap=plt.get_cmap('gray'))
+    plt.title('Original Image')
+    
+    plt.subplot(1,2,2)
+    plt.imshow(filt_img, cmap=plt.get_cmap('gray'))
+    plt.title('Filtered Image')
+    
+    plt.show()
+
